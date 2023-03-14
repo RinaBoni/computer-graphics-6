10d88f823fe473d1efa74a68c3c9ca727e51fd95
RinaBoni <83748388+RinaBoni@users.noreply.github.com>
2023-03-14 17:09:59 +0900

    Сдано
    Написано большенство коммов. Лучше еще немного разовраться, дописать их

--- DdaBresenham/DdaBresenham/Form1.Designer.cs
+++ DdaBresenham/DdaBresenham/Form1.Designer.cs
@@ -38,10 +38,10 @@
             this.label4 = new System.Windows.Forms.Label();
             this.DdaPictureBox = new System.Windows.Forms.PictureBox();
             this.BresenhamPictureBox = new System.Windows.Forms.PictureBox();
-            this.buttonDda = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.buttonBres = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.DdaPictureBox)).BeginInit();
@@ -66,7 +66,7 @@
             // 
             // x2TextBox
             // 
-            this.x2TextBox.Location = new System.Drawing.Point(54, 151);
+            this.x2TextBox.Location = new System.Drawing.Point(54, 171);
             this.x2TextBox.Margin = new System.Windows.Forms.Padding(2);
             this.x2TextBox.Name = "x2TextBox";
             this.x2TextBox.Size = new System.Drawing.Size(76, 20);
@@ -74,7 +74,7 @@
             // 
             // y2TextBox
             // 
-            this.y2TextBox.Location = new System.Drawing.Point(54, 191);
+            this.y2TextBox.Location = new System.Drawing.Point(54, 211);
             this.y2TextBox.Margin = new System.Windows.Forms.Padding(2);
             this.y2TextBox.Name = "y2TextBox";
             this.y2TextBox.Size = new System.Drawing.Size(76, 20);
@@ -103,7 +103,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 156);
+            this.label3.Location = new System.Drawing.Point(16, 176);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(23, 13);
@@ -113,7 +113,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(16, 196);
+            this.label4.Location = new System.Drawing.Point(16, 216);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(23, 13);
@@ -122,101 +122,100 @@
             // 
             // DdaPictureBox
             // 
-            this.DdaPictureBox.Location = new System.Drawing.Point(134, 11);
+            this.DdaPictureBox.Location = new System.Drawing.Point(165, 72);
             this.DdaPictureBox.Margin = new System.Windows.Forms.Padding(2);
             this.DdaPictureBox.Name = "DdaPictureBox";
-            this.DdaPictureBox.Size = new System.Drawing.Size(227, 308);
+            this.DdaPictureBox.Size = new System.Drawing.Size(300, 325);
             this.DdaPictureBox.TabIndex = 8;
             this.DdaPictureBox.TabStop = false;
             // 
             // BresenhamPictureBox
             // 
-            this.BresenhamPictureBox.Location = new System.Drawing.Point(362, 11);
+            this.BresenhamPictureBox.Location = new System.Drawing.Point(542, 72);
             this.BresenhamPictureBox.Margin = new System.Windows.Forms.Padding(2);
             this.BresenhamPictureBox.Name = "BresenhamPictureBox";
-            this.BresenhamPictureBox.Size = new System.Drawing.Size(227, 308);
+            this.BresenhamPictureBox.Size = new System.Drawing.Size(300, 325);
             this.BresenhamPictureBox.TabIndex = 9;
             this.BresenhamPictureBox.TabStop = false;
             // 
-            // buttonDda
+            // button1
             // 
-            this.buttonDda.Location = new System.Drawing.Point(134, 336);
-            this.buttonDda.Margin = new System.Windows.Forms.Padding(2);
-            this.buttonDda.Name = "buttonDda";
-            this.buttonDda.Size = new System.Drawing.Size(110, 27);
-            this.buttonDda.TabIndex = 10;
-            this.buttonDda.Text = "Построить ЦДА";
-            this.buttonDda.UseVisualStyleBackColor = true;
-            this.buttonDda.Click += new System.EventHandler(this.buttonDda_Click);
+            this.button1.Location = new System.Drawing.Point(165, 440);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 25);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Построить график";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(381, 321);
+            this.label5.Location = new System.Drawing.Point(539, 416);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(63, 13);
             this.label5.TabIndex = 11;
             this.label5.Text = "Брезенхем";
-            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(131, 321);
+            this.label6.Location = new System.Drawing.Point(163, 416);
             this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(31, 13);
             this.label6.TabIndex = 12;
             this.label6.Text = "ЦДА";
             // 
-            // buttonBres
+            // button2
             // 
-            this.buttonBres.Location = new System.Drawing.Point(384, 336);
-            this.buttonBres.Margin = new System.Windows.Forms.Padding(2);
-            this.buttonBres.Name = "buttonBres";
-            this.buttonBres.Size = new System.Drawing.Size(131, 28);
-            this.buttonBres.TabIndex = 10;
-            this.buttonBres.Text = "Построить Брезенхем";
-            this.buttonBres.UseVisualStyleBackColor = true;
-            this.buttonBres.Click += new System.EventHandler(this.buttonDda_Click);
+            this.button2.Location = new System.Drawing.Point(542, 440);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 25);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Построить график";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(11, 11);
+            this.label7.Location = new System.Drawing.Point(16, 34);
             this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(113, 13);
-            this.label7.TabIndex = 4;
-            this.label7.Text = "Введите координаты";
+            this.label7.Size = new System.Drawing.Size(166, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Введите значения x от 0 до 299\r\n";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(0, 35);
+            this.label8.Location = new System.Drawing.Point(186, 34);
             this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(124, 13);
-            this.label8.TabIndex = 4;
-            this.label8.Text = "макс 227 по x, 308 по y";
+            this.label8.Size = new System.Drawing.Size(166, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Введите значения у от 0 до 324";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(877, 514);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.buttonBres);
-            this.Controls.Add(this.buttonDda);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.BresenhamPictureBox);
             this.Controls.Add(this.DdaPictureBox);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label7);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.y2TextBox);
             this.Controls.Add(this.x2TextBox);
@@ -244,10 +243,10 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.PictureBox DdaPictureBox;
         private System.Windows.Forms.PictureBox BresenhamPictureBox;
-        private System.Windows.Forms.Button buttonDda;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Button buttonBres;
+        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
     }
--- DdaBresenham/DdaBresenham/Form1.cs
+++ DdaBresenham/DdaBresenham/Form1.cs
@@ -15,12 +15,6 @@ namespace DdaBresenham
     public partial class Form1 : Form
     {
         double x1, y1, x2, y2, Px, Py;
-
-        private void label5_Click(object sender, EventArgs e)
-        {
-
-        }
-
         Bitmap ddaBuf, bresBuf;
 
         public Form1()
@@ -31,14 +25,28 @@ namespace DdaBresenham
         // ЦДА
         private void notsymetricCDA()
         {
-            Px = x2 - x1;
-            Py = y2 - y1;
-            ddaBuf.SetPixel((int)Math.Round(x1), (int)Math.Round(y1), Color.Black);
-            while (x1 < x2)
+            if ((x1 < x2) || (x1 == x2)) {  //если х1 меньше х2 или равен ему
+                Px = x2 - x1;               //ширина прямой
+                Py = y2 - y1;               //высота прямой
+                ddaBuf.SetPixel((int)Math.Round(x1), (int)Math.Round(y1), Color.Black); //красим первый пиксель
+                while (x1 < x2)             //пока не дошли до конца по х
+                {
+                    x1 = x1 + 1.0;          //увеличиваем координату по х (увеличивам на единицу)
+                    y1 = y1 + Py / Px;      //увеличиваем координату по у (увеличиваем по формуле)
+                    ddaBuf.SetPixel((int)Math.Round(x1), (int)Math.Round(y1), Color.Black); //красим пиксель
+                }
+            }
+            else    //то же самое что и выше, но строим начиная с х2 (тк она(координата) меньше)
             {
-                x1 = x1 + 1.0;
-                y1 = y1 + Py / Px;
-                ddaBuf.SetPixel((int)Math.Round(x1), (int)Math.Round(y1), Color.Black);
+                Px = x1 - x2;
+                Py = y1 - y2;
+                ddaBuf.SetPixel((int)Math.Round(x2), (int)Math.Round(y2), Color.Black);
+                while (x2 < x1)
+                {
+                    x2 = x2 + 1.0;
+                    y2 = y2 + Py / Px;
+                    ddaBuf.SetPixel((int)Math.Round(x2), (int)Math.Round(y2), Color.Black);
+                }
             }
 
         }
@@ -46,62 +54,55 @@ namespace DdaBresenham
         // Брезенхем
         private void brezenhem()
         {
-            int deltaX = (int)Math.Abs(x2 - x1);
-            int deltaY = (int)Math.Abs(y2 - y1);
-            int signX = x1 < x2 ? 1 : -1;
-            int signY = y1 < y2 ? 1 : -1;
-            int error = deltaX - deltaY;
-            bresBuf.SetPixel((int)Math.Round(x2), (int)Math.Round(y2), Color.Black);
-            while (x1 != x2 || y1 != y2)
+            int deltaX = (int)Math.Abs(x2 - x1);    //ширина прямой
+            int deltaY = (int)Math.Abs(y2 - y1);    //высота прямой 
+            int signX = x1 < x2 ? 1 : -1;           //если х1 меньше х2 знак равен 1, если нет, знак равен -1
+            int signY = y1 < y2 ? 1 : -1;           //если у1 меньше у2 знак равен 1, если нет, знак равен -1
+            int error = deltaX - deltaY;            //
+            bresBuf.SetPixel((int)Math.Round(x2), (int)Math.Round(y2), Color.Black);    //красим последний пиксель
+            while (x1 != x2 || y1 != y2)            //пока не дошли до конца или по х, или по у
             {
-                bresBuf.SetPixel((int)Math.Round(x1), (int)Math.Round(y1), Color.Black);
+                bresBuf.SetPixel((int)Math.Round(x1), (int)Math.Round(y1), Color.Black);    //красим пиксель (при первом прохождении первый пиксель прямой, дальше остальные по увеличению)
                 int error2 = error * 2;
                 if (error2 > -deltaY)
                 {
                     error -= deltaY;
-                    x1 += signX;
+                    x1 += signX;                    //увеличиваем координату по х (увеличивам на единицу или минус единицу, в зависимость от того, чему равно signX)
                 }
                 if (error2 < deltaX)
                 {
                     error += deltaX;
-                    y1 += signY;
+                    y1 += signY;                    //увеличиваем координату по у (увеличивам на единицу или минус единицу, в зависимость от того, чему равно signУ)
                 }
             }
         }
 
-        private void buttonDda_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)  //кнопка нарисовать цда
         {
             ddaBuf = new Bitmap(DdaPictureBox.Width, DdaPictureBox.Height);
-/*            bresBuf = new Bitmap(BresenhamPictureBox.Width, BresenhamPictureBox.Height);
-*/
-            x1 = int.Parse(x1TextBox.Text);
-            y1 = int.Parse(y1TextBox.Text);
-            x2 = int.Parse(x2TextBox.Text);
-            y2 = int.Parse(y2TextBox.Text);
 
-            notsymetricCDA();
-/*            brezenhem();
-*/
+            x1 = int.Parse(x1TextBox.Text);     //получаем из текстбокса х1
+            y1 = int.Parse(y1TextBox.Text);     //получаем из текстбокса у1
+            x2 = int.Parse(x2TextBox.Text);     //получаем из текстбокса х2
+            y2 = int.Parse(y2TextBox.Text);     //получаем из текстбокса у2
+
+            notsymetricCDA();   //рисуем цда
+
             DdaPictureBox.Image = ddaBuf;
-/*            BresenhamPictureBox.Image = bresBuf;
-*/        }
+        }
 
-        private void buttonBres_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)  //кнопка нарисовать брезенхем
         {
-/*            ddaBuf = new Bitmap(DdaPictureBox.Width, DdaPictureBox.Height);
-*/         bresBuf = new Bitmap(BresenhamPictureBox.Width, BresenhamPictureBox.Height);
+            bresBuf = new Bitmap(BresenhamPictureBox.Width, BresenhamPictureBox.Height);
 
-            x1 = int.Parse(x1TextBox.Text);
-            y1 = int.Parse(y1TextBox.Text);
-            x2 = int.Parse(x2TextBox.Text);
-            y2 = int.Parse(y2TextBox.Text);
+            x1 = int.Parse(x1TextBox.Text);     //получаем из текстбокса х1
+            y1 = int.Parse(y1TextBox.Text);     //получаем из текстбокса у1
+            x2 = int.Parse(x2TextBox.Text);     //получаем из текстбокса х2
+            y2 = int.Parse(y2TextBox.Text);     //получаем из текстбокса у2
 
-/*            notsymetricCDA();
-*/          brezenhem();
+            brezenhem();    //рисуем брезенхем
 
-/*            DdaPictureBox.Image = ddaBuf;
-*/         BresenhamPictureBox.Image = bresBuf;
-
+            BresenhamPictureBox.Image = bresBuf;
         }
     }
 }
