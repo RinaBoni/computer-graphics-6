d4fbfaac4535eedd927cb6a211fb6fa6b474ce0a
RinaBoni <brisovae@mail.ru>
2023-05-25 23:17:17 +0900

    8

---
+++ lab8c#/histogramm/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace histogramm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.buttonLoadimage = new System.Windows.Forms.Button();
+            this.buttonCreatehistograms = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.Location = new System.Drawing.Point(14, 15);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(956, 390);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // buttonLoadimage
+            // 
+            this.buttonLoadimage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonLoadimage.Location = new System.Drawing.Point(803, 411);
+            this.buttonLoadimage.Name = "buttonLoadimage";
+            this.buttonLoadimage.Size = new System.Drawing.Size(113, 23);
+            this.buttonLoadimage.TabIndex = 2;
+            this.buttonLoadimage.Text = "Load image";
+            this.buttonLoadimage.UseVisualStyleBackColor = true;
+            this.buttonLoadimage.Click += new System.EventHandler(this.buttonLoadimage_Click);
+            // 
+            // buttonCreatehistograms
+            // 
+            this.buttonCreatehistograms.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonCreatehistograms.Location = new System.Drawing.Point(803, 440);
+            this.buttonCreatehistograms.Name = "buttonCreatehistograms";
+            this.buttonCreatehistograms.Size = new System.Drawing.Size(113, 23);
+            this.buttonCreatehistograms.TabIndex = 3;
+            this.buttonCreatehistograms.Text = "Create histograms";
+            this.buttonCreatehistograms.UseVisualStyleBackColor = true;
+            this.buttonCreatehistograms.Click += new System.EventHandler(this.buttonCreatehistograms_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBox1.Location = new System.Drawing.Point(14, 411);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(257, 107);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBox3.Location = new System.Drawing.Point(277, 411);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(257, 107);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBox4.Location = new System.Drawing.Point(540, 411);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(257, 107);
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(804, 468);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Scale:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(901, 468);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "%";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(844, 466);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(51, 20);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 530);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.buttonCreatehistograms);
+            this.Controls.Add(this.buttonLoadimage);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button buttonLoadimage;
+        private System.Windows.Forms.Button buttonCreatehistograms;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ lab8c#/histogramm/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace histogramm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            for(int i = 0; i < 256; i++)
+            {
+                R[i] = 0;
+                G[i] = 0;
+                B[i] = 0;
+            }
+        }
+        Image img;
+        int[] R = new int[256];
+        int[] G = new int[256];
+        int[] B = new int[256];
+        // загружаем картинку
+        private void buttonLoadimage_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog(this);
+
+            if (openFileDialog1.FileName == "openFileDialog1" || openFileDialog1.FileName == "")
+                return;
+
+            img = Image.FromFile(openFileDialog1.FileName);
+            pictureBox2.Image = ResizeImg(img, pictureBox2.Width, pictureBox2.Height);
+        
+        }
+        public Image ResizeImg(Image b, int nWidth, int nHeight)
+        {
+            Image result = new Bitmap(nWidth, nHeight);
+            using (Graphics g = Graphics.FromImage((Image)result))
+            {
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.DrawImage(b, 0, 0, nWidth, nHeight);
+                g.Dispose();
+            }
+            return result;
+        }
+
+        private void buttonCreatehistograms_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.FileName == "")
+                buttonLoadimage_Click(sender, e);
+            FillArrayRGB(img);
+        }
+
+        private void FillArrayRGB(Image img)
+        {
+            float scale = (float)numericUpDown1.Value / 100;
+            Color color;
+            Bitmap bmp = new Bitmap(openFileDialog1.FileName);
+
+            var w = bmp.Width;
+            var h = bmp.Height;
+            // перебираем картинку по пикселям
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    color = bmp.GetPixel(i, j);
+                    R[color.R] += 1;
+                    G[color.G] += 1;
+                    B[color.B] += 1;
+                }
+            }
+            // графики строятся в потоках
+            // передаем массивы с цветом
+            new Task(() => MakeHistogram(pictureBox1, R, Color.Red, scale)).Start();
+            new Task(() => MakeHistogram(pictureBox3, G, Color.Green, scale)).Start();
+            new Task(() => MakeHistogram(pictureBox4, B, Color.Blue, scale)).Start();
+        }
+        // отрисовка гистограммы
+        // передаем объект, в который передаем битмап, передаем массив по R G B, цвет, скейл
+        private void MakeHistogram(PictureBox pictureBox, int[] array, Color color, float scale)
+        {
+            double max = array.Max();// наибольшее число вхождения одного цвета
+            double size = (double)((pictureBox.Height - 1) / max);//максимальный размер от контейнера/ самый высокий график, одно деление
+            double value;
+            Bitmap bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
+            for(int i = 0; i < array.Count(); i++)
+            {
+                value = array[i] * size * scale;// берет число цвета * на деление * на масштаб
+                if(value > pictureBox.Height - 1)// если масштаб  больше 100, тогда  максимальный размер 
+                    value = pictureBox.Height - 1;
+                // отрисовываем каждую линию счетчика для гистограммы по высоте
+                for(int j = 0; j < value;j++)
+                {
+                    bmp.SetPixel(i, pictureBox.Height - 1 - j, color);
+                }
+            }
+            pictureBox.Image = bmp;
+        }
+    }
+}
