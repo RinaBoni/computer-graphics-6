fe7895b53e77e9c96139d125f255bf6ca71046a0
RinaBoni <83748388+RinaBoni@users.noreply.github.com>
2023-05-26 15:12:52 +0900

    Add files via upload

--- lab15c#/graphic15/Logic.cs
+++ lab15c#/graphic15/Logic.cs
@@ -22,6 +22,7 @@ namespace graphic15
 
             image = new Bitmap(newWidth, newHeight);
 
+            //отрисоввка первой картинки
             for (int X = 0; X < startWidth; X++)
             {
                 for (int Y = 0; Y < startHeight; Y++)
@@ -30,6 +31,8 @@ namespace graphic15
                 }
             }
 
+            //отрисовка второй картинки
+            //маленькие х и у координаты от которых идет наложение
             for (int X = 0; X < endWidth; X++)
             {
                 for (int Y = 0; Y < endHeight; Y++)
@@ -38,16 +41,18 @@ namespace graphic15
                 }
             }
 
+            //обраотка зоны наложения
+            //складывыет пиксели первой и второй картинки и делит на 2
             for (int X = x; X < startWidth; X++)
             {
                 for(int Y = y; Y < startHeight; Y++)
                 {
-                    var newA = (a.GetPixel(X, Y).A + b.GetPixel(X, Y).A) / 2;
-                    var newR = (a.GetPixel(X, Y).R + b.GetPixel(X, Y).R) / 2;
-                    var newG = (a.GetPixel(X, Y).G + b.GetPixel(X, Y).G) / 2;
-                    var newB = (a.GetPixel(X, Y).B + b.GetPixel(X, Y).B) / 2;
+                    var newA = (a.GetPixel(X, Y).A + b.GetPixel(X-x, Y-y).A) / 2;
+                    var newR = (a.GetPixel(X, Y).R + b.GetPixel(X-x, Y-y).R) / 2;
+                    var newG = (a.GetPixel(X, Y).G + b.GetPixel(X-x, Y-y).G) / 2;
+                    var newB = (a.GetPixel(X, Y).B + b.GetPixel(X-x, Y-y).B) / 2;
                     image.SetPixel(X, Y, 
-                        Color.FromArgb(newA, newR, newG, newB));
+                        Color.FromArgb(newA, newR, newG, newB));//новый цвет
                 }
             }
 
